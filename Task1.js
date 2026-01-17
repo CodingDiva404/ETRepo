f366ddafded84941bfd48f2ceb91cd254d7d9109
Shraddha Gupta <education.shraddha123@gmail.com>
2026-01-17 16:52:09 +0000

    Counter

--- Task1.js
+++ Task1.js
@@ -1,4 +1,15 @@
-#Task1
-#Simple-Counter
-#Todo
-#Context-API Counter
+// #Task1
+// #Simple-Counter
+// #Todo
+// #Context-API Counter
+
+
+// Task 2
+// React-Router and nested Routing 
+// Implement navigation and useParam
+// Nodejs application that give user to upload the txt file
+
+// Task 3
+// React application using Redux tool kit and which filter out the cricket list  
+// and search bar where it filter out using the input text
+// write test case using react test and jest test
---
+++ counter/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main'>
+          <button>+</button>
+          <h3>0</h3>
+          <button>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
