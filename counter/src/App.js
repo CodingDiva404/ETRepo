f4b4ce02dc50b411412ddf680a0b2a781bf881d8
Shraddha Gupta <education.shraddha123@gmail.com>
2026-01-18 05:53:35 +0000

    Counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [number , setNumber] = useState(0);
+
+  function increaseValue() {
+    return setNumber(number + 1)
+  }
+  function decreaseValue(params) {
+    if(number === 0) return;  //If count is 0, exit the function immediately.
+    setNumber(number - 1)
+  }
+
   return (
     <div className="App">
+      <h1>Counter</h1>
       <div className='main'>
-          <button>+</button>
-          <h3>0</h3>
-          <button>-</button>
+          <button onClick={increaseValue}>+</button>
+          <h3>{number}</h3>
+          <button onClick={decreaseValue}>-</button>
       </div>
     </div>
   );
