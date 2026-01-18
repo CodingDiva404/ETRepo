c8fbd0036ba74ce7ce40561fb85fab5f0efd7d5f
Shraddha Gupta <education.shraddha123@gmail.com>
2026-01-18 08:17:02 +0000

    Add Item

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,18 +3,30 @@ import { useState } from 'react';
 
 function App() {
 
-  let [add , setAdd] = useState();
-  let [remove , setRemove] = useState();
-  let [edit , setEdit] = useState();
-  let [input , setInput] = useState();
+  let [add, setAdd] = useState();
+  let [remove, setRemove] = useState();
+  let [edit, setEdit] = useState();
+  let [input, setInput] = useState();
+  let [task, setTask] = useState([]);
 
-  
+
   const handleInput = (e) => {
-    let userInput = setInput(e.target.value)
-    console.log(userInput)
+    let userInput = e.target.value
+    setInput(userInput)
   }
 
   const addTask = () => {
+    // To 
+    if (input.trim() === "") return;
+    setTask([...task, { text: input, id: Date.now() }]);
+    setInput("");
+  }
+
+  const editTask = () => {
+
+  }
+
+  const deleteTask = () => {
 
   }
 
@@ -23,22 +35,21 @@ function App() {
     <div className='todo'>
       <h1>Todo List</h1>
       <div className="input">
-        <input type="text" className="userInput" onChange={handleInput}/>
-        <button className="addBtn">Add Item</button>
+        <input type="text" className="userInput" onChange={handleInput} />
+        <button className="addBtn" onClick={addTask}>Add Item</button>
       </div>
 
       <div className="list">
         <ul>
-          <li>
-            <li>
-              <span className="todoText">Buy milk and eggs</span>
+          {task.map((t) => (
+            <li key={t.id}>
+              <span className="todoText">{t.text}</span>
               <div className="actions">
                 <button className="editBtn">Edit</button>
                 <button className="removeBtn">Remove</button>
               </div>
             </li>
-
-          </li>
+          ))}
         </ul>
       </div>
     </div>
